3f56bf4dcbbd5759b4a9f414a290c080943c28ae
indiag <youthrules99@gmail.com>
2014-08-10 12:55:49 +0100

    added corrected tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -14,7 +14,7 @@ describe('my app', function () {
             browser.get('index.html#/view2');
         });
 
-        it('should render view2 when user navigates to /view2'p
+        it('should render view2 when user navigates to /view2'
             , function () {
             expect(element.all(by.css('[ng-view] h4')).first().getText()).
                 toMatch(/Find a Pool Coach/);
@@ -30,7 +30,7 @@ describe('my app', function () {
 
 
         it('should render view3 when user navigates to /view3', function () {
-            expect(element.all(by.css('[ng-view] h3')).first().getText()).
+            expect(element.all(by.css('[ng-view] h1')).first().getText()).
                 toMatch(/Find Our Pool Locations/);
         });
 
@@ -45,7 +45,7 @@ describe('my app', function () {
 
         it('should render view4 when user navigates to /view4', function () {
             expect(element.all(by.css('[ng-view] h1')).first().getText()).
-                toMatch(/Your Exercise Statistics/);
+                toMatch(/Workout Data/);
         });
     });
 
@@ -72,7 +72,7 @@ describe('my app', function () {
         describe("index", function () {
 
             it('should display the correct title', function () {
-                expect(browser.getTitle()).toBe('Gymnasium App')
+                expect(browser.getTitle()).toBe('Pool App')
             });
         });
     });
@@ -129,37 +129,25 @@ describe('my app', function () {
 
         describe("Google maps function", function () {
 
-            it('should show correct text for gym location ', function () {
+            it('should show correct text for pool location ', function () {
 
                 var ptor = protractor.getInstance();
 
                 //This will not get the option required
 
-                expect(ptor.element(by.css('#l1')).getText()).toMatch('Cork Street');
+                expect(ptor.element(by.css('#l1')).getText()).toMatch('Parnell Street');
 
                 ptor.findElement(protractor.By.css('#addressinput option:nth-child(2)')).click();
                 ptor.findElement(protractor.By.css('#Button1')).click();
-                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Quarry Road');
+                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Arjun Nagar, Agra, India');
 
                 ptor.findElement(protractor.By.css('#addressinput option:nth-child(3)')).click();
                 ptor.findElement(protractor.By.css('#Button1')).click();
-                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Bishopstown Road');
+                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Malakpet,Hyderbad, India');
 
-                ptor.findElement(protractor.By.css('#addressinput option:nth-child(4)')).click();
-                ptor.findElement(protractor.By.css('#Button1')).click();
-                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Henry Street');
 
-                ptor.findElement(protractor.By.css('#addressinput option:nth-child(5)')).click();
-                ptor.findElement(protractor.By.css('#Button1')).click();
-                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Ennis Road');
 
-                ptor.findElement(protractor.By.css('#addressinput option:nth-child(6)')).click();
-                ptor.findElement(protractor.By.css('#Button1')).click();
-                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Rindoon Park');
 
-                ptor.findElement(protractor.By.css('#addressinput option:nth-child(7)')).click();
-                ptor.findElement(protractor.By.css('#Button1')).click();
-                expect(ptor.findElement(protractor.By.css('#l1')).getText()).toMatch('Scarlett Street, Drogheda');
             });
 
         });
@@ -181,8 +169,8 @@ describe('my app', function () {
 
                 ptor.findElements(protractor.By.repeater("coach in coachList"));
 
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Bonnie');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Bonnie Dowling');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Balaji');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Balaji O Shea');
 
             });
 
@@ -191,24 +179,24 @@ describe('my app', function () {
                 var ptor = protractor.getInstance();
 
                 ptor.findElement(protractor.By.css('#sortBy option:nth-child(2)')).click();
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('George');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('George Best');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('James');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('James Mason');
 
                 ptor.findElement(protractor.By.css('#sortBy option:nth-child(3)')).click();
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Phil');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Phil Lynnott');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Henry');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Hungry Henry');
 
                 ptor.findElement(protractor.By.css('#sortBy option:nth-child(4)')).click();
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Joe');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Joe Brolly');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Balaji');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Balaji Murphy');
 
                 ptor.findElement(protractor.By.css('#sortBy option:nth-child(5)')).click();
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Kevin');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Kevin Heffernan');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Balaji');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Enda Kenny');
 
                 ptor.findElement(protractor.By.css('#sortBy option:nth-child(6)')).click();
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Seamus');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Seamus Heaney');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('karthik');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('karthik Jones');
 
             });
 
@@ -216,53 +204,43 @@ describe('my app', function () {
 
                 var ptor = protractor.getInstance();
 
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('mary');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Mary');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('balaji');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Balaji');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('bonnie');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Bonnie');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Bonnie Dowling');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Jimmy');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Jimmy');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Jimmy Johnston');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Dwyer');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Mick');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Mick 0 Dwyer');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Karthik');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('karthik');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('karthin');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Bono');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Bono');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Mick');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Kiran');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Kiran');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Kieran');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Watson');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('James');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('James Watson');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Latif');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Latif');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Gatif');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('James');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('James');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('James Watson');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Pavan');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Pavan');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Aavan');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Heffernan');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Kevin');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Kevin H');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Venkat');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Venkat');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Vikas');
 
                 ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Pauling');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Linus');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Linus P');
-
-                ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Paidi');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Paidi');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Paidi O Shea');
-
-                ptor.findElement(protractor.By.id("ngsearch")).clear();
-                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Seamus');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Seamus');
-                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Seamus Heaney');
+                ptor.findElement(protractor.By.id("ngsearch")).sendKeys('Vivek');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).toMatch('Vivek');
+                expect(ptor.findElement(protractor.By.css('#myFirstName')).getText()).not.toMatch('Livek');
 
             });
         });
